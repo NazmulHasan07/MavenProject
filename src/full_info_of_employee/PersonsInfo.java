70699dadf045e88bc4bf8814b572f2f6804a8e24
Nazmul Hasan <nhrhandsome@gmail.com>
2017-12-16 22:51:32 -0500

    I have uploaded one file here

---
+++ src/full_info_of_employee/PersonsInfo.java
@@ -0,0 +1,22 @@
+//*****************************************************
+// PersonsInfo.java   Author: Nazmul Hasan
+// Demonstrates the basic structure of java application.
+//*******************************************************
+
+
+package full_info_of_employee;
+
+public class PersonsInfo {
+
+	public static void main(String[] args) {
+		System.out.println("Employee:                            Amjad Hossain");
+		System.out.print("\nEmployee Id:                         AH5078");
+		System.out.print("\nPosition:                            Sr. Accountant");
+        System.out.print("\nStart date:                          06/01/2012");
+        System.out.print("\nSalary:                              $65000.00");
+        System.out.print("\nAchievement                          Best Employee of the Year 2016");
+        System.out.print("\nCertified License:                   Yes");
+
+	}
+
+}
